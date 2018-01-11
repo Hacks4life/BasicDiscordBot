9e79312b10140aed26c40fb1a05624e848bc00a4
Tomas Pimentel <Tomashacksforlife@gmail.com>
2018-01-11 13:39:05 -0500

    Create BasicDiscordBot.py

---
+++ BasicDiscordBot.py
@@ -0,0 +1,42 @@
+# These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+
+# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
+client = Bot(description="New Bot by DJHacks", command_prefix="!", pm_help = True)
+
+# This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
+# Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+	print('--------')
+	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+	print('--------')
+	print('Use this link to invite {}:'.format(client.user.name))
+	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+	print('--------')
+	print('Support Discord Server: https://discord.gg/dQGqRYj')
+	print('Github Link: https://github.com/Hacks4life/BasicDiscordBot')
+	print('--------')
+	print('Created by DJHacks')
+
+# This is a basic example of a call and response command. You tell it do "this" and it does it.
+@client.command()
+async def ping(*args):
+
+	await client.say(":ping_pong: Pong!")
+	await asyncio.sleep(3)
+	await client.say(":warning: This bot was created by **DJHacks**, it seems to be under construction")
+# After you have modified the code, feel free to delete the line above (line 33) so it does not keep popping up everytime you initiate the ping commmand.
+	
+client.run('PUT YOUR BOT TOKEN HERE')
+
+# Basic Bot was created by DJHacks
+# Please join this Discord server if you need help: https://discord.gg/dQGqRYj
+# Please modify the parts of the code where it asks you to. Example: The Prefix or The Bot Token
+# This is a starter bot to show you what the python language can do in Discord.
+# Thank you for using this and don't forget to star my repo on GitHub! [Repo Link: https://github.com/Hacks4life/BasicDiscordBot]
